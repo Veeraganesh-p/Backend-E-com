19e9f16bff26c605b9958fac32a470b8310be125
Your Name <you@example.com>
2025-12-18 00:10:10 +0530

    Replace old backend files with new backend

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const ProductRoute = require("./routes/productRoute")
 const dotenv = require("dotenv");
 const connectdb = require("./config/db");
+const cors = require("cors");
+
 dotenv.config();
 connectdb();
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
